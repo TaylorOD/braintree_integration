940e52b9c9c3beb68a0e975779329d291b3125ad
taylorod <taylorsunwantedemails@gmail.com>
2020-12-07 13:09:25 -0600

    added documentation to controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,11 +1,11 @@
 class CheckoutController < ApplicationController
 
-  def new
+  def new # Generates new client token
     @client_token = gateway.client_token.generate
     render json: { client_token: @client_token }, status: :created
   end
 
-  def gateway
+  def gateway # Env vars which are stored in my rails credentials file
     Braintree::Gateway.new(
     :environment => :sandbox,
     :merchant_id => "#{Rails.application.credentials.braintree[:merchant_id]}",
@@ -14,9 +14,10 @@ class CheckoutController < ApplicationController
     )
   end
 
-  def transaction
+  def transaction # transaction code
     nonce_from_the_client = params[:payment_method_nonce]
 
+    # creates transaction using nonce
     result = gateway.transaction.sale(
       :amount => "10.00",
       :payment_method_nonce => nonce_from_the_client,
@@ -25,6 +26,7 @@ class CheckoutController < ApplicationController
       }
     )
 
+    # happy/sad path for transaction result
     if result.success?
       settled_transaction = result.transaction
 
